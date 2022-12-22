467275f9c8e45b31de1cec0904d3759e33bac1f3
Broyzdev0 <broyzdesign240904@gmail.com>
2022-12-22 09:48:16 +0700

    update

--- timer-promise.mjs
+++ timer-promise.mjs
@@ -2,5 +2,5 @@ import timers from "timers/promises";
 
 
 for await (const startTime of timers.setInterval( 1000, "ignored")){
-    console.info('Start time at ${new Date()} ');
+    console.info('Start time at' + new Date());
 }
